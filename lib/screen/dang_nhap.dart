86b0482d5f677bbb349e039f21af4fd39c6238e4
Đào Ngọc Duy Thanh <95920353+DuyThanh2601@users.noreply.github.com>
2021-12-22 02:25:50 -0800

    Update dang_nhap.dart

---
+++ lib/screen/dang_nhap.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+import 'dang_ky.dart';
+
+class dangnhap extends StatelessWidget {
+  const dangnhap({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget thaotac = thaotacIcon();
+    return new Scaffold(
+      body: new Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            begin: Alignment.centerLeft,
+            end: new Alignment(1.0, 0.0),
+            colors: [Colors.blue, Colors.red.shade400],
+          ),
+        ),
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(top: 100.0, bottom: 10.0),
+              child: Center(
+                child: new Column(
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      child: new Hero(
+                        tag: 'hero',
+                        child: CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          radius: 48.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: Colors.white,
+                      width: 0.5,
+                      style: BorderStyle.solid),
+                ),
+              ),
+              padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+              child: new Row(
+                children: <Widget>[
+                  new Padding(
+                    padding:
+                        EdgeInsets.only(top: 7.0, bottom: 10.0, right: 00.0),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                  ),
+                  new Expanded(
+                    child: TextField(
+                      textAlign: TextAlign.left,
+                      style: new TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Tên tài khoản',
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                      color: Colors.white,
+                      width: 0.5,
+                      style: BorderStyle.solid),
+                ),
+              ),
+              padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+              child: new Row(
+                children: <Widget>[
+                  new Padding(
+                    padding:
+                        EdgeInsets.only(top: 7.0, bottom: 10.0, right: 00.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: Colors.white,
+                    ),
+                  ),
+                  new Expanded(
+                    child: TextField(
+                      obscureText: true,
+                      textAlign: TextAlign.left,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Mật khẩu',
+                        hintStyle: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  thaotac
+                ],
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 30.0),
+              alignment: Alignment.center,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new FlatButton(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20.0, horizontal: 20.0),
+                        color: Colors.lightBlue,
+                        child: Text(
+                          "Đăng nhập",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => home()),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 40.0, right: 40.0, top: 10.0),
+              alignment: Alignment.center,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new FlatButton(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 20.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        "Quên mật khẩu?",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () => {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            new Container(
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(
+                  left: 40.0, right: 40.0, top: 10.0, bottom: 20.0),
+              alignment: Alignment.center,
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(
+                    child: new FlatButton(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 20.0),
+                      color: Colors.transparent,
+                      child: Text(
+                        "Bạn chưa có tài khoản? Đăng ký",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => dangky()),
+                        )
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class thaotacIcon extends StatefulWidget {
+  const thaotacIcon({Key? key}) : super(key: key);
+
+  @override
+  State<thaotacIcon> CreateState() => _thaotacIconState();
+}
+
+class _thaotacIconState extends State<thaotacIcon> {
+  @override
+  Widget build(BuildContext context) {
+    bool out = true;
+    return Row(children: [
+      IconButton(
+        onPressed: () {
+          setState() {}
+        },
+        icon: out ? Icon(Icons.visibility) : Icon(Icons.visibility_off),
+        color: Colors.white,
+      )
+    ]);
+  }
+}
